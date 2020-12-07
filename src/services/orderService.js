7bc6b70698883553a09544b7eca076a2cf290ddb
dragonouou <shenglanouou@gmail.com>
2020-12-06 18:48:30 -0800

    add order service and recipeService

---
+++ src/services/orderService.js
@@ -0,0 +1,19 @@
+const url = "http://localhost:8080.com/api"
+
+export const findAllOrders = () =>
+    fetch(`${url}/orders`)
+        .then(response => response.json())
+
+export const createOrder = (newOrder) =>
+    fetch(`${url}/orders`,{
+        method:"POST",
+        body:JSON.stringify(newOrder),
+        headers:{
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export default {
+    findAllOrders, createOrder
+}
--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -3,7 +3,6 @@ const url ="https://api.spoonacular.com/recipes"
 const apiKey = "apiKey=c8b6eac40e34427886d68e4409e9ae56"
 const maxNum = "number=2"
 
-
 export const findRecipesByTitle = (title) =>
     fetch(`${searchUrl}?${apiKey}&query=${title}&${maxNum}`)
         .then(response => response.json())
@@ -15,3 +14,5 @@ export const findRecipesByTitle = (title) =>
 export const findRecipeById = (recipeId) =>
     fetch(`${url}/${recipeId}/information?${apiKey}`)
         .then(response => response.json())
+
+

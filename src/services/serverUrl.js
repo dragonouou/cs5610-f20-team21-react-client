1bbbe000684ad97b326adb1eb150d19621b7f9d4
dragonouou <shenglanouou@gmail.com>
2020-12-15 02:40:23 -0800

    change to remote server

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8080/api"
-// export const server = "https://homekitchenserver.herokuapp.com/api"
+// export const server = "http://localhost:8080/api"
+export const server = "https://homekitchenserver.herokuapp.com/api"

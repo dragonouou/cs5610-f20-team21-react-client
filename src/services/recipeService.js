b063ead825376e557e70d3f602597ef157f08c60
dragonouou <shenglanouou@gmail.com>
2020-12-15 03:19:52 -0800

    import first

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,9 +1,9 @@
+import {server} from "./serverUrl";
+
 const searchUrl = "https://api.spoonacular.com/recipes/complexSearch"
 const url ="https://api.spoonacular.com/recipes"
 const apiKey = "apiKey=c8b6eac40e34427886d68e4409e9ae56"
 const maxNum = "number=4"
-
-import {server} from "./serverUrl";
 const serverUrl = server
 
 export const findRecipesByTitle = (title) =>
